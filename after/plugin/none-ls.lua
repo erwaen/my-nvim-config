a11a99c08cb5d9a42fe0920688ff4eba630a890d
erik <erikwasmosy98@gmail.com>
2024-01-01 14:33:47 -0300

    add new file for none-ls config

---
+++ after/plugin/none-ls.lua
@@ -0,0 +1,14 @@
+local null_ls = require("null-ls")
+
+
+-- keymaps
+
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.completion.spell,
+    },
+})
+
