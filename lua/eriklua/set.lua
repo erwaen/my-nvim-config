56e7a7d2a649a685751b8d9bbe8dc093a7c24020
erik <erikwasmosy98@gmail.com>
2024-01-01 13:06:08 -0300

    update set.lua

--- lua/eriklua/set.lua
+++ lua/eriklua/set.lua
@@ -1,5 +1,3 @@
-
--- Set options
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
@@ -14,6 +12,24 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
 vim.opt.termguicolors = true
 
+
 vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+vim.opt.cursorline = true
